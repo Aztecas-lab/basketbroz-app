5712f806c5d7264a541cc7032cd35a31a68fe90f
jhlin <imjustaup@gmail.com>
2023-11-01 01:18:48 +0800

    feat: add android splash screen

--- android/app/src/main/java/com/sendbird/uikit/reactnative/sample/MainActivity.java
+++ android/app/src/main/java/com/sendbird/uikit/reactnative/sample/MainActivity.java
@@ -1,21 +1,26 @@
 package com.sendbird.uikit.reactnative.sample;
 
 import com.facebook.react.ReactActivity;
+
 import android.os.Bundle;
+
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "SendbirdUIKitSample";
-  }
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "SendbirdUIKitSample";
+    }
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        androidx.core.splashscreen.SplashScreen.installSplashScreen(this); // native splash screen which will be skipped
+        SplashScreen.show(this);
+        super.onCreate(null);
+    }
 }
